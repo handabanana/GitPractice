3d39bdca36b6df2c8505be0d8766ebda91ef1544
Hannah Jackowski <JackowHR20@pwcs-edu.org>
2019-05-06 13:38:03 -0400

    fixed the dummy doo doo

--- RandomNumber.java
+++ RandomNumber.java
@@ -11,6 +11,24 @@ public class RandomNumber
 	public RandomNumber()
 	{
 		Scanner scan = null;
+		try
+		{
+			System.out.println( "Type the smallest integer you would like." );
+			scan = new Scanner( System.in );
+			int smallest = scan.next();
+			System.out.println( "Now, type the largest integer you would like." );
+			scan = new Scanner( System.in );
+			int largest = scan.next();
+			int random = Math.random() * ( largest - smallest + 1 ) + largest;
+			System.out.println( "Your random number is: " + random );
+		}
+		finally
+		{
+			if ( scan != null )
+			{
+				scan.close();
+			}
+		}
 	}
 
 }
